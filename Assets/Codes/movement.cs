198f19ec0f9a3127cc84442f2f7746d53a6a1fa6
Flame260 <162753205+Flame260@users.noreply.github.com>
2024-09-16 16:05:49 -0400

    intial

---
+++ Assets/Codes/movement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float accel = 1;
+    public float hor = 1;
+    public float vert = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 currentDir=Dir()
+            
+    }
+    void Fixedupdate()
+    {
+
+    }
+}
