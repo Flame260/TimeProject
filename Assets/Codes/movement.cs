36e1cfeec4752f89dcb87356830d71cc29e51a7f
Flame260 <162753205+Flame260@users.noreply.github.com>
2024-09-16 16:46:27 -0400

    Did nothing

--- Assets/Codes/movement.cs
+++ Assets/Codes/movement.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class movement : MonoBehaviour
 {
     public float accel = 1;
-    public float hor = 1;
-    public float vert = 1;
+    public float horM = 1;
+    public float vertM = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +16,24 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 currentDir=Dir()
-            
+        
     }
-    void Fixedupdate()
+    private void FixedUpdate()
     {
-
+        Vector3 currentDir = Dir();
+        currentDir.x *= horM;
+        currentDir.y *= vertM;
+        transform.Translate(Dir() * accel);
+    }
+    public Vector3 Dir()
+    {
+        //unity default axis provide a value between -1 to 1
+        //in the case of vertial,thats w=1 amd s=-1 
+        float y = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Horizontal");
+        //construct our vector out of the vertical and horizontal axis
+        Vector3 myDir = new Vector3(x, y, 0);
+        //return the value
+        return myDir;
     }
 }
