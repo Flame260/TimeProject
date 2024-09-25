ea2dc8e5d72bf619de98ba2dfd2335ab4b09351c
Flame260 <162753205+Flame260@users.noreply.github.com>
2024-09-25 15:34:29 -0400

    ai and shooting update

--- Assets/Codes/bullet.cs
+++ Assets/Codes/bullet.cs
@@ -6,18 +6,24 @@ using UnityEngine;
 public class bullet : MonoBehaviour
 {
     public Gamemanager gamemanager;
+    private bool hit;
     public void Start()
     {
         gamemanager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<Gamemanager>();
+        hit = false;
     }
     // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if(hit)return;
+        
         Debug.Log(collision.gameObject.tag);
         if (collision.gameObject.tag == "Collectible")
         {
             Destroy(collision.gameObject);
             gamemanager.AddScore();
+            hit= true;
+             Destroy(gameObject);
         }
     }
     
